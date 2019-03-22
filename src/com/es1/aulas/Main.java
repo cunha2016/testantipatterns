a1fe4d29968db0241ea72206fbab358e9d011c37
Carlos Cunha <cunha.cas@gmail.com>
2019-03-22 21:28:11 +0000

    Init

---
+++ src/com/es1/aulas/Main.java
@@ -0,0 +1,18 @@
+package com.es1.aulas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //This is not good stuff
+
+        int x = 10;
+
+        int z = 20;
+
+        int w = 50;
+
+        System.out.println("...");
+
+    }
+}
